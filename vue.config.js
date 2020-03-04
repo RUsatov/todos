9bcf99fd086e9f2055266f4f67503da6f907aef3
Roman Usatov <R.Usatov76@gmail.com>
2020-03-04 22:56:20 +0300

    express install

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/todo/' : '/',
+
   css: {
     loaderOptions: {
       sass: {
         prependData: `@import "~@/assets/styles/variables.scss";`
       },
+    }
+  },
+
+  pluginOptions: {
+    express: {
+      shouldServeApp: false,
+      serverDir: './server'
     }
   }
 }
